c4f5cf73eaf36aee3bf7ef515d445d842fc660bb
mark miranda <notmarkmiranda@gmail.com>
2017-11-07 09:10:40 -0700

    Finish UL enum

--- db/migrate/20171107033338_create_user_leagues.rb
+++ db/migrate/20171107033338_create_user_leagues.rb
@@ -3,7 +3,7 @@ class CreateUserLeagues < ActiveRecord::Migration[5.1]
     create_table :user_leagues do |t|
       t.references :user, foreign_key: true
       t.references :league, foreign_key: true
-      t.integer :role
+      t.integer :role, default: 0
 
       t.timestamps
     end
